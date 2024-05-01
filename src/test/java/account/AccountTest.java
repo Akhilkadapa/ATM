ce854742b138af4fc0b03032396607545a3ff25a
Kadapa <akhilsantosh.kadapa@bt.com>
2024-05-01 09:58:32 +0100

    removed the repition out of the account test class

--- src/test/java/account/AccountTest.java
+++ src/test/java/account/AccountTest.java
@@ -1,37 +1,49 @@
 package account;
 
-// AccountTest.java
-
 import exception.InsufficientFundsException;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class AccountTest {
-    @Test
-    public void testWithdraw() throws InsufficientFundsException {
-        Account account = new Account(1000);
-        account.withdraw(500);
-        assertEquals(500, account.getBalance(), 0.001);
+    private Account account;
+
+    @Before
+    public void setUp() {
+        account = new Account(1000);
     }
 
-    @Test(expected = IllegalArgumentException.class)
+    @Test
+    public void testWithdraw() {
+        try {
+            account.withdraw(500);
+            assertEquals(500, account.getBalance(), 0.001);
+        } catch (InsufficientFundsException e) {
+            fail("Unexpected InsufficientFundsException occurred");
+        }
+    }
+
+    @Test
     public void testWithdrawNegativeAmount() {
-        Account account = new Account(1000);
         try {
             account.withdraw(-500);
+            fail("Expected IllegalArgumentException was not thrown");
+        } catch (IllegalArgumentException e) {
+            // Expected IllegalArgumentException, test passes
         } catch (InsufficientFundsException e) {
-            throw new RuntimeException(e);
+            fail("Unexpected InsufficientFundsException occurred");
         }
     }
 
-    @Test(expected = InsufficientFundsException.class)
-    public void testWithdrawInsufficientFunds() throws InsufficientFundsException {
-        Account account = new Account(1000);
-
+    @Test
+    public void testWithdrawInsufficientFunds() {
+        try {
             account.withdraw(5000);
-
+            fail("Expected InsufficientFundsException was not thrown");
+        } catch (InsufficientFundsException e) {
+            // Expected InsufficientFundsException, test passes
         }
-
     }
-
+}
